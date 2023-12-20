9cd427e873957a7f3c26331842aaef8d9c74bfce
SeoSeunghun <proto_seo@naver.com>
2023-12-20 10:36:46 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 8.23 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -2,38 +2,25 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] jobs) {
-        Arrays.sort(jobs, (o1, o2) -> {
-            if (o1[0] == o2[0]) {
-                return o1[1] - o2[1];
-            }
-            return o1[0] - o2[0];
-        });
+        Arrays.sort(jobs, (o1, o2) -> o1[0] - o2[0]);
         int jobCount = jobs.length;
         int answer = 0;
         
         int idx = 0;
-        int now = 0;
-        int[] run = {0, 0};
+        int[] runJob = {0, 0};
         PriorityQueue<int[]> waitQueue = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
         for (int i = 0; i <= 500000; i++) {
-           	if (idx < jobCount && jobs[idx][0] <= i) {
-                int[] selectedJob = jobs[idx++];
-                if (run[1] > i) {
-                    waitQueue.add(selectedJob);
+           	while (idx < jobCount && jobs[idx][0] <= i) {
+                waitQueue.add(jobs[idx++]);
+            }
+            if (!waitQueue.isEmpty()) {
+                if (runJob[1] > i) {
             	    continue;
             	}
-              	waitQueue.add(selectedJob);
 	          	int[] waitedJob = waitQueue.poll();
 	            answer += i - waitedJob[0] + waitedJob[1];
-	            run = new int[]{i, i + waitedJob[1]};
-            } else if (!waitQueue.isEmpty()) {
-                if (run[1] > i) {
-            	    continue;
-            	}
-               	int[] waitedJob = waitQueue.poll();
-	            answer += i - waitedJob[0] + waitedJob[1];
-	            run = new int[]{i, i + waitedJob[1]};
-            }     
+	            runJob = new int[]{i, i + waitedJob[1]};   
+            }  
         }
         return answer / jobCount;
     }
