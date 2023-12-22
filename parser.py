ce7523f594ee9ecae8386b3170ebdd8277541f2a
SeoSeunghun <proto_seo@naver.com>
2023-12-22 22:29:28 +0900

    update workflow yaml file

--- parser.py
+++ parser.py
@@ -1,34 +1,56 @@
 import os
 import re
+import sys
 
+
+def parse_problem_info(file_name):
+    file_splits = file_name.split('/')
+    problem_type = 'PRGMS' if file_splits[0] == '프로그래머스' else 'BOJ'
+    problem_number = file_splits[2].split('.')[0]
+    problem_name = file_splits[2].split('.')[1].strip()
+    language = file_splits[3].split('.')[1].strip()
+    return [problem_type, problem_number, problem_name, language]
+
+
+if len(sys.argv) != 2:
+    print("Please input target problem file directory")
+    exit(1)
+
+target_problem_file_name = sys.argv[1]
 script_dir = os.path.dirname(os.path.abspath(__file__))
-relative_path = "algorithm-study/README.md"
-file_path = os.path.join(script_dir, relative_path)
+read_file_path = os.path.join(script_dir, "algorithm-study/README.md")
 
 start_week_pattern = re.compile('<td>(\d+)주차</td>')
-problem_number_pattern = re.compile('/(\d+)"')
 
-file = open(file_path, 'r')
-result = open(os.path.join(script_dir, "result.txt"), 'w')
-str_list = []
-for line in file:
-    str_list.append(line.strip())
+readme_file = open(read_file_path, 'r')
 
-for i in range(0, len(str_list)):
-    line = str_list[i]
-    if not start_week_pattern.search(line):
+problems_by_week = {}
+is_week_start = False
+week_number = 0
+for line in readme_file:
+    if start_week_pattern.search(line):
+        is_week_start = True
+        week_number = start_week_pattern.findall(line)[0]
+        problems_by_week[week_number] = []
         continue
 
-    week_number = start_week_pattern.findall(line)[0]
-    result.write(week_number + '주차' + '\n')
-    i += 1
-    next_line = str_list[i]
-    while "</tr>" not in next_line:
-        problem_number = problem_number_pattern.findall(next_line)[0]
-        if "programmers" in next_line:
-            result.write('PRGMS ' + problem_number + '\n')
-        elif "acmicpc" in next_line:
-            result.write('BOJ ' + problem_number + '\n')
-        i += 1
-        next_line = str_list[i]
-    result.write('\n')
+    if "</tr>" in line:
+        is_week_start = False
+        continue
+
+    if is_week_start:
+        problem_number = re.findall('/(\d+)"', line)[0]
+        if "programmers" in line:
+            problems_by_week[week_number].append('PRGMS ' + problem_number)
+        elif "acmicpc" in line:
+            problems_by_week[week_number].append('BOJ ' + problem_number)
+
+target_problem_info = parse_problem_info(target_problem_file_name)
+for key in problems_by_week:
+    type = target_problem_info[0]
+    number = target_problem_info[1]
+    if type + ' ' + number in problems_by_week[key]:
+        print(' '.join(target_problem_info))
+        exit(0)
+
+print('NOT FOUND')
