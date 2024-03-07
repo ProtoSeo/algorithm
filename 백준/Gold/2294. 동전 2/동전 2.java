b289e1a4566dffd0c444f83613ce569829758110
SeoSeunghun <proto_seo@naver.com>
2024-03-07 14:23:58 +0900

    [Gold V] Title: 동전 2, Time: 136 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(stk.nextToken());
+        int k = Integer.parseInt(stk.nextToken());
+        int[] coins = new int[n];
+        for (int i = 0; i < n; i++) {
+            coins[i] = Integer.parseInt(br.readLine());
+        }
+
+        int[] dp = new int[k + 1];
+        Arrays.fill(dp, 100005);
+        dp[0] = 0;
+        for (int coin : coins) {
+            for (int i = coin; i <= k; i++) {
+                dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+            }
+        }
+        System.out.println(dp[k] == 100005 ? -1 : dp[k]);
+    }
+}
