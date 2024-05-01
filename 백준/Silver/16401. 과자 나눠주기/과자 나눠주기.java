0008250701486c302d78ce74810b21c7e31459f8
SeoSeunghun <proto_seo@naver.com>
2024-05-01 13:56:45 +0900

    [Silver II] Title: 과자 나눠주기, Time: 1376 ms, Memory: 126240 KB -BaekjoonHub

---
+++ 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        var br = new BufferedReader(new InputStreamReader(System.in));
+        var stk = new StringTokenizer(br.readLine());
+        int m = Integer.parseInt(stk.nextToken());
+        int n = Integer.parseInt(stk.nextToken());
+
+        var snack = new int[n];
+        stk = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            snack[i] = Integer.parseInt(stk.nextToken());
+        }
+        Arrays.sort(snack);
+
+        int l = 1;
+        int r = 1_000_000_001;
+        int answer = 0;
+        while (l < r) {
+            int mid = (l + r) / 2;
+            long count = 0;
+            for (int i = 0; i < n; i++) {
+                count += snack[i] / mid;
+            }
+            if (count >= m) {
+                l = mid + 1;
+                answer = Math.max(answer, mid);
+            } else {
+                r = mid;
+            }
+        }
+        System.out.println(answer);
+    }
+}
